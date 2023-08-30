3ee3baf0b1fd71ac9ac12b78aa4c544f32d273c3
mrwim <willem.wiltenburg@inholland.nl>
2023-08-30 09:14:16 +0200

    Assignment 1

--- src/main/java/nl/inholland/fundamentals/App.java
+++ src/main/java/nl/inholland/fundamentals/App.java
@@ -1,8 +1,16 @@
 package nl.inholland.fundamentals;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.printf("%s %d%n", name, age);
+        scanner.close();
     }
 
     public String printGreeting(String name) {
